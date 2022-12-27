bef7d82a203def3691fae49a2834b5fa29adf51d
Alon Zilberman <alonzilberman@icloud.com>
2022-12-28 00:15:41 +0200

    added function to print header of syscalls

--- Main.py
+++ Main.py
@@ -5,10 +5,12 @@ parser = argparse.ArgumentParser(description="RoboTracer")
 parser.add_argument("filepath", metavar="F", type=str, nargs="+", help="The file path RoboTracer will run")
 fp = parser.parse_args().filepath[0]
 
-process = sp.check_output(['strace', "./"+fp], stderr=sp.STDOUT)
+process = sp.check_output(['strace', "./"+fp], stderr=sp.STDOUT, timeout=10) # running strace on the process with timeout of 10 seconds
 
 def get_syscalls(proc):
-    syscalls = proc.decode("utf-8").split("\n")
-    return syscalls
+    syscalls = proc.decode("utf-8").split("\n") # get all syscalls from strace
+    return syscalls[:-1] # remove final exit message
 
-print(get_syscalls(process))
+def print_header_syscalls(syscalls):
+    for i in range(min(16, len(syscalls))): # iterating over 16 first syscalls(or less if not enough)
+        print(syscalls[i])

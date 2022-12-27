8cd9964487475ed44c1b7a92eaa457b868a68cc6
Alon Zilberman <alonzilberman@icloud.com>
2022-12-27 23:39:53 +0200

    small fix to getting filepath

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import subprocess as sp
 
 parser = argparse.ArgumentParser(description="RoboTracer")
 parser.add_argument("filepath", metavar="F", type=str, nargs="+", help="The file path RoboTracer will run")
-fp = parser.parse_args().filepath
+fp = parser.parse_args().filepath[0]
 
 def get_syscalls():
     syscalls = sp.Popen(['strace', "./"+fp]).stdout.read().decode("utf-8").split('\n')

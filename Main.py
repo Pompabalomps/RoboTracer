c15d9aa8afcbe99941bf024df6435fc714eaa84b
Alon Zilberman <alonzilberman@icloud.com>
2022-12-27 23:37:54 +0200

    testing getting syscalls

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 import argparse
+import subprocess as sp
 
 parser = argparse.ArgumentParser(description="RoboTracer")
-
 parser.add_argument("filepath", metavar="F", type=str, nargs="+", help="The file path RoboTracer will run")
-
 fp = parser.parse_args().filepath
+
+def get_syscalls():
+    syscalls = sp.Popen(['strace', "./"+fp]).stdout.read().decode("utf-8").split('\n')
+    print(syscalls)
+
+get_syscalls()

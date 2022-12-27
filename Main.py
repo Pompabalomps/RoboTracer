e79da19a79e256cc9a535aeeedd636910278630f
Alon Zilberman <alonzilberman@icloud.com>
2022-12-27 23:56:13 +0200

    changed order of operations

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ parser = argparse.ArgumentParser(description="RoboTracer")
 parser.add_argument("filepath", metavar="F", type=str, nargs="+", help="The file path RoboTracer will run")
 fp = parser.parse_args().filepath[0]
 
-def get_syscalls():
-    syscalls = sp.Popen(['strace', "./"+fp], stdout=sp.PIPE).stdout.read().decode("utf-8").split('\n')
-    print(syscalls)
+process = sp.Popen(['strace', "./"+fp], stdout=sp.PIPE, stderr=sp.PIPE)
 
-get_syscalls()
+def get_syscalls(proc):
+    syscalls = proc.stdout.read().encode("utf-8").split("\n")
+    return syscalls
+
+print(get_syscalls(process))

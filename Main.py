830f9d6ba14ff77806f1eb5e09aed7d02619d4b4
Alon Zilberman <alonzilberman@icloud.com>
2022-12-28 00:09:28 +0200

    added stderr support #2

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description="RoboTracer")
 parser.add_argument("filepath", metavar="F", type=str, nargs="+", help="The file path RoboTracer will run")
 fp = parser.parse_args().filepath[0]
 
-process = sp.check_output(['strace', "./"+fp], stderr=sp.PIPE)
+process = sp.check_output(['strace', "./"+fp], stderr=sp.STDOUT)
 
 def get_syscalls(proc):
     syscalls = proc.stderr.read().decode("utf-8").split("\n")
